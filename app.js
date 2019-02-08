39f39f5a0390d5ee888be0bddd9ec62ca502c043
samashtar <sam.ashtar@flatironschool.com>
2019-02-08 05:01:38 -0500

    cors server created

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ window.addEventListener('load', () => {
     let lat
     let temperatureDescription = document.querySelector('.temperature-description')
     let temperatureDegree = document.querySelector('.temperature-degree')
-    let locationTimezon = document.querySelector('.location-timezone')
+    let locationTimezone = document.querySelector('.location-timezone')
 
-
+    // make sure the cors server is running 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude
             lat = position.coords.latitude
-            const api = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/9e103a3c9907677e215a32d27b95bcff/${lat},${long}`
+            const api = `http://localhost:8080/https://api.darksky.net/forecast/9e103a3c9907677e215a32d27b95bcff/${lat},${long}`
             fetch(api).then(res => res.json()).then(data => {
-                console.log(data)
                 const {
                     temperature,
                     summary
                 } = data.currently
-
                 temperatureDegree.textContent = temperature
+                temperatureDescription.textContent = summary
+                locationTimezone.textContent = data.timezone
             })
         })
     } else {
